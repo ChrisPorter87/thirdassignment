b5b90b2b45959ccd5adc9fa926c37ceaf9237322
Chris Porter <cap8095@gmail.com>
2022-03-20 17:06:44 -0400

    fixing links

--- Develop/script.js
+++ Develop/script.js
@@ -78,43 +78,7 @@ function characterTypes(
       retVal + charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
   return retVal;
-
-  //If user wants upper case then include them in password
-  // var uppercase;
-  // var lowercase;
-  // var numbers;
-  // var specialCharacters;
-  // if (upperCase) {
-  //   return getUpper();
-  // } else if (lowerCase) {
-  //   return getLower();
-  // } else if (numbers) {
-  //   return getNumber();
-  // } else if (specialCharacters) {
-  //   return getCharacter();
-  // } else {
-  //   return "";
-  // }
 }
-
-// var getRandom = {
-//   lower: getLower,
-//   upper: getUpper,
-//   number: getNumber,
-//   symbol: getCharacter,
-// };
-// function getLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-// function getUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-// function getNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-// function getCharacter() {
-//   return String.fromCharCode(Math.floor(Math.random() * 125) + 131);
-// }
 
 // Write password to the #password input
 function writePassword(lower, upper, number, symbol, length) {
